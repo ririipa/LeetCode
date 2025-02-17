44dee9daa5dbf938bf8e2a8efd0802b6a9d5b870
rKive01 <>
2025-02-17 23:07:15 +0000

    my solution ig :)

---
+++ 3174. Clear Digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        #create an empty stack 
+        stack = []
+
+        #loop through each character
+        for cahr in s:
+            #if digit remove last letter from stack
+            if cahr.isdigit():
+                if stack:
+                    stack.pop()
+        #if letter add to stack
+            else:
+                stack.append(cahr)
+
+                #join all in stack and make final string
+        return ''.join(stack)
