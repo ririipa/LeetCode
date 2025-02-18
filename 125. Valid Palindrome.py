cf5cf14ed9b67fe545f237bc17220c9230d20af0
rKive01 <>
2025-02-18 10:08:54 +0000

    return True if palindrome else false!

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alphanumeric = ""
+        for char in s:
+            if char.isalnum():
+                alphanumeric += char.lower()
+
+        return alphanumeric == alphanumeric[::-1]
